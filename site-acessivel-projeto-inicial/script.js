de4a71e9f12c4c7f6322727043d30aa64ae9bbb2
guilherme <168210658+GUILHERME165@users.noreply.github.com>
2025-09-07 00:56:37 -0300

    Add files via upload

---
+++ site-acessivel-projeto-inicial/script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function(){
+const botaoDeAcessibilidade = document.getElementById('botão-acessibilidade')
+const opcoesDeAcessibilidade = document.getElementById('opçoes-acessibilidade')
+
+botaoDeAcessibilidade.addEventListener('click', function (){
+ botaoDeAcessibilidade.classList.toggle('rotação-botão');
+ opcoesDeAcessibilidade.classList.toggle('apresentar-lista')
+
+ const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+ botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+
+})
+
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    const alternaContraste = document.getElementById('alternar-contraste');
+
+    let tamanhoAtualFonte = 1;
+
+     aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+     });
+
+     diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+
+    alternaContraste.addEventListener('click', function(){
+         document.body.classList.toggle('alto-contraste')
+ });
+});
+
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
